f1ed6ba6eb8b8ce0667f5579f825929d46536c04
RustedNature <133128777+RustedNature@users.noreply.github.com>
2024-05-14 14:54:06 +0200

    Projektdateien hinzufügen.

---
+++ SpaceInvaders/GameWindow.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceInvaders
+{
+    internal class GameWindow : Form
+    {
+        private readonly System.Timers.Timer gameTimer;
+        private int playerY = 300;
+        private int playerX = 400;
+
+        public GameWindow()
+        {
+            Width = 800;
+            Height = 600;
+            BackColor = Color.Black;
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Text = "Space Invaders";
+
+            gameTimer = new()
+            {
+                Interval = 1000 / 30
+            };
+            gameTimer.Elapsed += GameLoop;
+            gameTimer.Start();
+        }
+
+        private void GameLoop(object? sender, EventArgs e)
+        {
+            GameUpdate();
+
+            Invalidate();
+        }
+
+        private void GameUpdate()
+        {
+            playerY++;
+            playerX--;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.Clear(BackColor);
+            e.Graphics.FillRectangle(Brushes.White, playerX - 25, playerY - 25, 50, 50);
+
+            base.OnPaint(e);
+        }
+    }
+}
