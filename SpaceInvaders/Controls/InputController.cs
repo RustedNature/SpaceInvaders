bc0ace8628be42444961e60a49fd291a8e875478
RustedNature <133128777+RustedNature@users.noreply.github.com>
2024-05-15 10:19:56 +0200

    Added a InputController based on BitArray Added a KeyIndexEnum

---
+++ SpaceInvaders/Controls/InputController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceInvaders.Controls
+{
+    internal static class InputController
+    {
+        private static readonly BitArray keys = new(Enum.GetValues(typeof(KeyIndex)).Length);
+
+        public static BitArray Keys => keys;
+
+        public static void SetKey(Keys key)
+        {
+            switch (key)
+            {
+                case System.Windows.Forms.Keys.Left:
+                    Keys[((int)KeyIndex.Left)] = true;
+                    break;
+
+                case System.Windows.Forms.Keys.Right:
+                    Keys[((int)KeyIndex.Right)] = true;
+                    break;
+
+                case System.Windows.Forms.Keys.A:
+                    Keys[((int)KeyIndex.A)] = true;
+                    break;
+
+                case System.Windows.Forms.Keys.D:
+                    Keys[((int)KeyIndex.D)] = true;
+                    break;
+
+                case System.Windows.Forms.Keys.Space:
+                    Keys[((int)KeyIndex.Space)] = true;
+                    break;
+            }
+        }
+
+        public static void ResetKey(Keys key)
+        {
+            switch (key)
+            {
+                case System.Windows.Forms.Keys.Left:
+                    Keys[((int)KeyIndex.Left)] = false;
+                    break;
+
+                case System.Windows.Forms.Keys.Right:
+                    Keys[((int)KeyIndex.Right)] = false;
+                    break;
+
+                case System.Windows.Forms.Keys.A:
+                    Keys[((int)KeyIndex.A)] = false;
+                    break;
+
+                case System.Windows.Forms.Keys.D:
+                    Keys[((int)KeyIndex.D)] = false;
+                    break;
+
+                case System.Windows.Forms.Keys.Space:
+                    Keys[((int)KeyIndex.Space)] = false;
+                    break;
+            }
+        }
+    }
+}
---
+++ SpaceInvaders/Controls/KeyIndex.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceInvaders.Controls
+{
+    internal enum KeyIndex
+    {
+        A = 0,
+        D = 1,
+        Left = 2,
+        Right = 3,
+        Space = 4
+    }
+}
