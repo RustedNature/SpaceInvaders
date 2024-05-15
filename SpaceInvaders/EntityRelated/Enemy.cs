b97638d6570f1eed0b7215b927076b74d4d1f152
RustedNature <133128777+RustedNature@users.noreply.github.com>
2024-05-15 10:21:28 +0200

    Added first simple move logic

--- SpaceInvaders/EntityRelated/Enemy.cs
+++ SpaceInvaders/EntityRelated/Enemy.cs
@@ -8,9 +8,38 @@ namespace SpaceInvaders.EntitiyRelated
 {
     internal class Enemy : Entity
     {
+        private float maxX;
+        private float minX;
+        private bool isMoveingLeft = true;
+        private float moveSpeed = 50f;
+
         public Enemy(int posX, int posY)
             : base("C:\\Users\\Nico\\source\\repos\\C#\\SpaceInvaders\\SpaceInvaders\\Assets\\Enemy.png", posX, posY)
         {
+            maxX = posX + 50;
+            minX = posX - 50;
+        }
+
+        public void Move()
+        {
+            if (isMoveingLeft)
+            {
+                Coord.X -= moveSpeed * ((float)GameWindow.deltaTime);
+                if (minX > Coord.X)
+                {
+                    isMoveingLeft = false;
+                }
+            }
+
+            if (!isMoveingLeft)
+            {
+                Coord.X += moveSpeed * ((float)GameWindow.deltaTime);
+
+                if (maxX < Coord.X)
+                {
+                    isMoveingLeft = true;
+                }
+            }
         }
     }
 }
