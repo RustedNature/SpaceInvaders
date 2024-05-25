bf31508514ca587bf0db506f0c3899f2997427a0
RustedNature <133128777+RustedNature@users.noreply.github.com>
2024-05-25 18:31:24 +0200

    Added Bullet class

---
+++ SpaceInvaders/EntityRelated/Bullet.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.DirectoryServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceInvaders.EntityRelated
+{
+    internal class Bullet : Entity
+    {
+        private float moveSpeed = 250f;
+
+        private static string spritePath = Assets.Assets.AssetsPath + "\\bullet.png";
+
+        public Bullet(float posX, float posY) : base(SpritePath, posX, posY)
+        {
+        }
+
+        public float MoveSpeed { get => moveSpeed; set => moveSpeed = value; }
+        public static string SpritePath { get => spritePath; set => spritePath = value; }
+
+        public override void Move()
+        {
+            if (Coord.Y <= 50)
+            {
+                EntityHandler.MarkForRemove(this);
+                return;
+            }
+            Coord.Y -= MoveSpeed * (float)GameWindow.DeltaTime;
+        }
+
+        ~Bullet()
+        {
+            Debug.WriteLine(this.ToString() + " destroyed");
+        }
+    }
+}
