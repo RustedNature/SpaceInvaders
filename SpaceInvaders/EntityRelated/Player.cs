cb40373f72239c079b4b0026fe92d7a737a668ed
RustedNature <133128777+RustedNature@users.noreply.github.com>
2024-05-25 18:30:26 +0200

    Updated class added Shooting logic

--- SpaceInvaders/EntityRelated/Player.cs
+++ SpaceInvaders/EntityRelated/Player.cs
@@ -1,5 +1,6 @@
 ﻿using SpaceInvaders.Controls;
 using System.Collections;
+using SpaceInvaders.EntityRelated;
 
 namespace SpaceInvaders.EntityRelated
 {
@@ -7,28 +8,38 @@ namespace SpaceInvaders.EntityRelated
     {
         private string name;
         private readonly float moveSpeed = 100f;
+        private static readonly string spritePath = Assets.Assets.AssetsPath + "\\panzer.png";
+        private bool isShooting = false;
 
         public Player(string name, int posX, int posY)
-            : base(Assets.Assets.AssetsPath + "\\panzer.png", posX, posY)
+            : base(SpritePath, posX, posY)
         {
             Name = name;
         }
 
         public string Name { get => name; set => name = value; }
 
-        public void Move()
+        public static string SpritePath => spritePath;
+
+        public override void Move()
         {
             BitArray keys = InputController.Keys;
-            if (keys[(int)KeyIndex.Left] || keys[(int)KeyIndex.A])
+            if ((keys[(int)KeyIndex.Left] || keys[(int)KeyIndex.A]) && Coord.X > 0)
             {
-                Coord.X -= moveSpeed * (float)GameWindow.deltaTime;
+                Coord.X -= moveSpeed * (float)GameWindow.DeltaTime;
             }
-            else if (keys[(int)KeyIndex.Right] || keys[(int)KeyIndex.D])
+            else if ((keys[(int)KeyIndex.Right] || keys[(int)KeyIndex.D]) && Coord.X < GameWindow.ScreenWidth - Sprite.SpriteImage.Width)
             {
-                Coord.X += moveSpeed * (float)GameWindow.deltaTime;
+                Coord.X += moveSpeed * (float)GameWindow.DeltaTime;
             }
-            if (keys[(int)KeyIndex.Space])
+            if (keys[(int)KeyIndex.Space] && !isShooting)
             {
+                EntityHandler.CreateBullet(Coord.X, Coord.Y);
+                isShooting = true;
+            }
+            else
+            {
+                isShooting = false;
             }
         }
     }
