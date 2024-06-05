c5408aa035c800a5edb3770114c158fe150aab31
RustedNature <133128777+RustedNature@users.noreply.github.com>
2024-06-05 11:59:59 +0200

    Refractored

--- SpaceInvaders/Renderer.cs
+++ SpaceInvaders/Renderer.cs
@@ -9,22 +9,25 @@ namespace SpaceInvaders
 {
     internal static class Renderer
     {
-        private static Bitmap bufferMap = new Bitmap(GameWindow.ScreenWidth, GameWindow.ScreenHeight);
-        private static Graphics bufferGraphics = Graphics.FromImage(bufferMap);
+        private static Bitmap bufferMap = new(GameWindow.ScreenWidth, GameWindow.ScreenHeight);
+        private static Graphics bufferGraphics = Graphics.FromImage(BufferMap);
 
-        internal static void Render(PaintEventArgs e)
+        public static Bitmap BufferMap { get => bufferMap; set => bufferMap = value; }
+        public static Graphics BufferGraphics { get => bufferGraphics; set => bufferGraphics = value; }
+
+        internal static void Render(PaintEventArgs e, Color BackColor)
         {
-            bufferGraphics.Clear(Color.Black);
+            BufferGraphics.Clear(BackColor);
             RenderEntities();
 
-            e.Graphics.DrawImage(bufferMap, 0, 0);
+            e.Graphics.DrawImage(BufferMap, 0, 0);
         }
 
         private static void RenderEntities()
         {
             EntityManager.Entities.ForEach(e =>
             {
-                bufferGraphics.DrawImage(e.Sprite.SpriteImage, e.Coord.X, e.Coord.Y, e.Sprite.SpriteImage.Width, e.Sprite.SpriteImage.Height);
+                BufferGraphics.DrawImage(e.Sprite.SpriteImage, e.Coord.X, e.Coord.Y, e.Sprite.SpriteImage.Width, e.Sprite.SpriteImage.Height);
                 //Debug.WriteLine($"{e} drawn at X:{e.Coord.X} Y:{e.Coord.Y} and its {e.Sprite.SpriteImage.Width}x{e.Sprite.SpriteImage.Height}");
             });
         }
